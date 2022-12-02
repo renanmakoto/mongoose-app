4a4514236dd72f438ac8315a008bf4e5f10a97e3
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2022-12-02 09:06:53 -0300

    Add files via upload

--- controls/linkControl.js
+++ controls/linkControl.js
@@ -1,38 +1,38 @@
 const docModelLink = require('../models/docModelLink');
 
-const redirect = async (req, res) => {
+const redirect = async (req, res, next) => {
     let title = req.params.title;
-
     try {
-
-        let doc = await docModelLink.findOne({ click: "test" })
-
+        let doc = await docModelLink.findOne({ title })
         console.log(doc);
-
-        res.redirect(doc.url);
-
+        if (doc) {
+            res.redirect(doc.url);
+        } else {
+            next()
+        }
     } catch (error) {
-
         res.send(error);
-
     }
 };
 
 const addLink = async (req, res) => {
-
     let link = new docModelLink(req.body)
-
     try {
-
         let doc = await link.save()
         res.send("Link added succesfully.");
-
     } catch (error) {
-
         res.render('index', { error, body: req.body });
-
     }
-
 };
 
-module.exports = { redirect, addLink };
+const allLinks = async (req, res) => {
+
+    try {
+        let links = await docModelLink.find({});
+        res.send(links);
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+module.exports = { redirect, addLink, allLinks };
--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const linkControl = require('../controls/linkControl');
 
+router.get('/all', linkControl.allLinks);
+
 router.get('/:title', linkControl.redirect);
 
 router.post('/', express.urlencoded({ extended: true }), linkControl.addLink);
