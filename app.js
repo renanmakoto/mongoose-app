966b5784e49e88bf9dac9a12de892f372f4a826b
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2025-06-04 07:56:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.connect('mongodb://localhost/mongoose-app', {
 let db = mongoose.connection
 
 db.on("error", () => { console.log("There has been an error.") })
-db.once("open", () => { console.log("Database loaded.") });
+db.once("open", () => { console.log("Database loaded.") })
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'templates'));
